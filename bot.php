b53b208eb98b6f57ba437cd7683ca42175f0d7b3
Elshod <elshodatc1116@gmail.com>
2023-11-24 17:48:00 +0500

    Bot ChatID2 qo'shildi

--- bot.php
+++ bot.php
@@ -8,8 +8,13 @@
     
     $website="https://api.telegram.org/bot".$botToken;
     $chatId=952410592;  
+    $chatId2=1363698146;
     $params=[
         'chat_id'=>$chatId, 
+        'text'=>"ATKO o'quv markaziga web sayt orqali murojat \n 👤 Ism: ".$name."\n 📞 Tel: ".$phone."\n 📧 Email: ".$email."\n 📝 Matn: ".$text,
+    ];
+    $params2=[
+        'chat_id'=>$chatId2, 
         'text'=>"ATKO o'quv markaziga web sayt orqali murojat \n 👤 Ism: ".$name."\n 📞 Tel: ".$phone."\n 📧 Email: ".$email."\n 📝 Matn: ".$text,
     ];
     $ch = curl_init($website . '/sendMessage');
@@ -20,6 +25,17 @@
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
     curl_close($ch);
+    
+    $ch = curl_init($website . '/sendMessage');
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ($params2));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    
+    
     header("location: ./contact.php?send=true");
     
 ?>
