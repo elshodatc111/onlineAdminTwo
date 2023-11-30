50706776bc08c3f77eb47e0b907f53bd0ec678e5
Elshod Musurmonov <elshodatc1116@gmail.com>
2023-11-30 22:08:18 +0500

    Kamchiliklar to'g'irlandi

--- admin/users.php
+++ admin/users.php
@@ -10,7 +10,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Admin</title>
-        <link rel="shortcut icon" href="../assets/compiled/svg/favicon.svg" type="image/x-icon">
+        <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon">
         <link rel="stylesheet" href="../assets/compiled/css/app.css">
         <link rel="stylesheet" href="../assets/compiled/css/app-dark.css">
         <link rel="stylesheet" href="../assets/compiled/css/iconly.css">
